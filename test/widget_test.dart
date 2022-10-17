eeae504e2f03bbe0e46cf8c54e17ed7d2b06761e
arielr <roasso@gmail.com>
2022-10-18 00:40:55 +0100

    temp fix of test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,4 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pictionary/main.dart';
 
-void main() {}
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+  });
+}
